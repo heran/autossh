16752506da2a70527003b4cc4154d15eb680b86c
温强 <wenqiang@wenqiangdeMacBook-Pro.local>
2014-05-12 23:29:07 +0800

    install bug

--- install.sh
+++ install.sh
@@ -8,21 +8,16 @@ if [ ! -w "$testPath" ]; then
 	exit
 fi 
 
-rm -fr ~/.ssh/autossh.exp
+rm -fr /usr/bin/autossh.exp
 rm -fr /usr/bin/autossh
 
 #copy files
-cp autossh.exp ~/.ssh/autossh.exp
+cp autossh.exp /usr/bin/autossh.exp
 cp autossh.sh /usr/bin/autossh
-
-if [[ ! -s  ~/.ssh/ssh_passwd ]]; then
-	cp ssh_passwd ~/.ssh/ssh_passwd
-fi
 
 #change permission
 chmod +x /usr/bin/autossh
-chmod 777 ~/.ssh/ssh_passwd
-chmod 777 ~/.ssh/autossh.exp
+chmod +x /usr/bin/autossh.exp
 
 echo -e "\033[32;1mautossh install successs\033[0m"
 echo -e "\033[32;1mauthor:heran\033[0m"
